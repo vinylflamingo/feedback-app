1e52520c50d622bab4e7aa1debcce57bc71a46b5
Frank Costoya <fcostoyaprograms@gmail.com>
2024-05-30 15:11:05 -0400

    changing deploy to erase docker images.

--- api/update_packages.sh
+++ api/update_packages.sh
@@ -5,4 +5,5 @@ pip install --no-cache-dir -r /api/requirements.txt
 
 # Restart the FastAPI app
 pkill -f uvicorn
-uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload 
